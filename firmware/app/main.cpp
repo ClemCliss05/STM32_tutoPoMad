2c5344cdce30fd5d6855d34f5435ba01d7fa77e7
ClemCliss05 <clissonclement05@gmail.com>
2026-06-09 02:30:33 +0100

    tuto1.3: Configure and Run the debugger

--- firmware/app/main.cpp
+++ firmware/app/main.cpp
@@ -1,14 +1,52 @@
-int main(void)
+#include <cstdint>
+
+constexpr uint32_t DELAY_ON  = 20000;
+constexpr uint32_t DELAY_OFF = 100000;
+
+int main()
 {
-    int i;
+    uint32_t i = 0;
+    uint32_t j = 0;
 
-    *(int *)0x40021014 |=  (0x01 <<17U);
-    *(int *)0x48000000 &= ~(0xC00);
-    *(int *)0x48000000 |=  (0x01 <<10U);
+    uint8_t state = 0;
 
-    while(1)
+    *reinterpret_cast<volatile uint32_t*>(0x40021014) |= (1U << 17U);
+
+    *reinterpret_cast<volatile uint32_t*>(0x48000000) &= ~(0x3U << 10U);
+    *reinterpret_cast<volatile uint32_t*>(0x48000000) |=  (0x1U << 10U);
+
+    while (true)
     {
-    	*(int *)0x48000014 ^= 0x00000020U;
-    	for (i=0; i<100000; i++);
+        switch (state)
+        {
+            case 0:
+            {
+                *reinterpret_cast<volatile uint32_t*>(0x48000014) &= ~0x20U;
+
+                ++i;
+
+                if (i > DELAY_OFF)
+                {
+                    i = 0;
+                    ++j;
+                    state = 1;
+                }
+                break;
+            }
+
+            case 1:
+            {
+                *reinterpret_cast<volatile uint32_t*>(0x48000014) |= 0x20U;
+
+                ++i;
+
+                if (i > DELAY_ON)
+                {
+                    i = 0;
+                    state = 0;
+                }
+                break;
+            }
+        }
     }
 }
